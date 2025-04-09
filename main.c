da9569a49168780e40f8ab2edc4da6d1879185ce
NattanSilva <natanxboy@gmail.com>
2025-04-09 19:43:33 -0300

    fix: organizanção do projeto

--- main.c
+++ main.c
@@ -350,6 +350,51 @@ int cadastrarAlunoEMaterias(T_Sala * sala) {
   return 1;
 }
 
+/**
+ * @brief Remove um aluno da sala pelo RGM.
+ * 
+ * Esta função procura o aluno com o RGM informado. Se encontrado:
+ * - Libera todas as matérias (lista encadeada) desse aluno.
+ * - Remove o aluno da lista sequencial (vetor).
+ * - Atualiza a quantidade total de alunos na sala.
+ * 
+ * @param sala Ponteiro para a estrutura T_Sala.
+ * @param rgm  RGM do aluno a ser removido.
+ * @return int 1 se remover com sucesso, 0 se não encontrar.
+*/
+int removerAlunoPorRGM(T_Sala * sala, int rgm) {
+  if (listaDeAlunosVazia(sala)) {
+    printf("Nenhum aluno cadastrado.\n");
+    return 0;
+  }
+
+  int i, j;
+  for (i = 0; i <= sala->n; i++) {
+    if (sala->alunos[i].rgm == rgm) {
+      // Libera lista encadeada de matérias
+      T_NO * atual = sala->alunos[i].cabeca;
+      T_NO * temp;
+      while (atual != NULL) {
+        temp = atual;
+        atual = atual->prox;
+        free(temp); // libera cada nó
+      }
+
+      // Desloca os alunos à esquerda (remove o aluno do vetor)
+      for (j = i; j < sala->n; j++) {
+        sala->alunos[j] = sala->alunos[j + 1];
+      }
+
+      sala->n--; // atualiza o número de alunos
+      printf("Aluno com RGM %d removido com sucesso!\n", rgm);
+      return 1;
+    }
+  }
+
+  printf("Aluno com RGM %d não encontrado.\n", rgm);
+  return 0;
+}
+
 void menu(T_Sala * sala) {
   int opcao = 0;
 
@@ -366,6 +411,7 @@ void menu(T_Sala * sala) {
   switch (opcao) {
     case 1:
       printf("-----------------------------------------------\n");
+      printf("Cadastro de alunos e materias\n");
       cadastrarAlunoEMaterias(sala);
       menu(sala);
       break;
@@ -387,6 +433,14 @@ void menu(T_Sala * sala) {
     case 4:
       printf("-----------------------------------------------\n");
       printf("Remocao de alunos\n");
+      int rgmRemocao;
+      printf("Digite o RGM do aluno a ser removido: ");
+      scanf("%d", &rgmRemocao);
+      removerAlunoPorRGM(sala, rgmRemocao);  // Chama a função de remoção
+      printf("-----------------------------------------------\n");
+      printf("Listagem atualizada\n");
+      listarALunos(sala); // Listagem atualizada
+      menu(sala);
       break;
     case 5:
       printf("-----------------------------------------------\n");
